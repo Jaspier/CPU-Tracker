b6acc461c43f988c6938174d384b4c1518fd3768
Jaspier <dungoallen349@gmail.com>
2021-10-07 22:04:17 +0100

    Fetching the dynamic system stats by time interval

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,22 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// Run every 2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then(info => {
+    document.getElementById('cpu-usage').innerText = info + '%';
+  });
+
+  // CPU Free
+  cpu.free().then(info => {
+    document.getElementById('cpu-free').innerText = info + '%';
+  });
+
+  // Uptime
+  document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+}, 2000);
+
 // CPU model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -17,3 +33,14 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 mem.info().then(info => {
   document.getElementById('mem-total').innerText = info.totalMemMb;
 });
+
+// Show days, hours, minutes, sec (uptime given in seconds)
+function secondsToDhms(uptime) {
+  uptime = +uptime;
+  const days = Math.floor(uptime / (3600 * 24));
+  const hours = Math.floor((uptime % (3600 * 24)) / 3600);
+  const minutes = Math.floor((uptime % 3600) / 60);
+  const seconds = Math.floor(uptime % 60);
+
+  return `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
+}
